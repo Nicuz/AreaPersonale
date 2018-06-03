af7c32e46e535104875ba3e8b4bc3a27354c8ea9
Matt <am.monteleone.am@gmail.com>
2018-06-03 17:44:09 +0200

    Aggiornate API

--- iliad-api/server.js
+++ iliad-api/server.js
@@ -12,6 +12,8 @@ app.get('/', function (req, res) {
     var email_confirm = req.query.email_confirm;
     var new_password = req.query.new_password;
     var new_password_confirm = req.query.new_password_confirm;
+    var info = req.query.info;
+    var doc = req.query.doc;
 
     var data_store = {};
     data_store["iliad"] = {};
@@ -115,10 +117,8 @@ app.get('/', function (req, res) {
                         data_store["iliad"]["sim"][0] = 'Attivazione avvenuta correttamente';
 
                     res.send(data_store);
-                    console.log(data_store);
 
                 });
-
             }
         });
     } else if (userid != undefined && password != undefined) {
@@ -158,179 +158,211 @@ app.get('/', function (req, res) {
                     var array1 = [];
 
                     results.each(function (i, result) {
+
                         data_store["iliad"]["user"] = {};
                         data_store["iliad"]["validation"] = {};
                         data_store["iliad"]["shipping"] = {};
+                        data_store["iliad"]["sim"] = {};
 
-                        // list user_name, user_id, user_numtell
-                        var nav = $(result)
-                            .find('div.current-user').first().text().split('\n')
+                        var nav = $(result).find('div.current-user').first().text().split('\n')
                         var user_name = nav[1].split('  ').join('')
                         var user_id = nav[2].split('  ').join('').replace('ID utente: ', '')
                         var user_numtell = nav[3].split('  ').join('').replace('Numero: ', '');
-                        var array = []
-                        var offer = $(result)
+
+                        var array = [];
+                        var array2 = [];
+                        var array3 = [];
+                        $(result)
                             .find('h2.title')
                             .each(function (index, element) {
                                 array = array.concat([$(element).text()]);
                             });
-                        var offer = array[0].split('\n')[1].split(' ').join('')
-
                         $(result)
                             .find('div.grid-l')
                             .find('div.step__text')
                             .each(function (index, element) {
-                                array = array.concat([$(element).find('h4.step__text__title').text()]);
+                                array2 = array2.concat([$(element).find('h4.step__text__title').text()]);
                             });
-                        var validation = array[0]
+                        $(result)
+                            .find('div.grid-l')
+                            .find('div.step__text')
+                            .each(function (index, element) {
+                                array3 = array3.concat([$(element).find('a').text()]);
+                            });
 
-                        var orderdate = $(result)
-                            .find('div.step__text').first().text().split('\n')
+                        var title = $(result).find('div.form-activation').text().split('   ').join('').split('\n')
+                        var orderdate = $(result).find('div.step__text').first().text().split('\n')
+                        var tracking = $(result).find('a.red').attr('href')
+                        var activation = $(result).find('p.explain').text().split('   ').join('').split('  ').join(' ').split('\n')
+                        var check = $(result).find('input').attr('name');
+                        var order_shipped = $(result).find('div.step__text').find('p').text()
+
+                        activation = activation[1];
+                        title = title[1];
+
+                        var offer = array[0].split('\n')[1].split('   ').join('')
                         var order_date = orderdate[2].split('   ').join('')
                         var date = orderdate[3].split('   ').join('')
+                        var tracking_text = array3[2]
+                        var validation = array2[0]
+                        var preparazione = array2[1]
+                        var spedizione = array2[2]
 
-                        var preparazione = array[1]
-
-
-                        try {
-
-                            results.each(function (i, result) {
-                                data_store["iliad"]["sim"] = {};
-
-                                var activation = $(result)
-                                    .find('p.explain').text().split('   ').join('').split('\n')
-                                activation = activation[1];
-
-                                var title = $(result)
-                                    .find('div.form-activation').text().split('   ').join('').split('\n')
-                                title = title[1];
-
-
-                                data_store["iliad"]["sim"][0] = title;
-                                data_store["iliad"]["sim"][1] = activation;
-
-                            });
-
-                        } catch (Exception) {}
-
-                        try {
-                            var spedizione = array[2]
-                            var tracking = $(result)
-                                .find('a.red').attr('href')
-                            var order_shipped = $(result)
-                                .find('div.step__text').find('p').text()
-                            data_store["iliad"]["shipping"][0] = spedizione;
-                            data_store["iliad"]["shipping"][1] = order_shipped;
-                            data_store["iliad"]["shipping"][2] = tracking;
-                        } catch (Exception) {}
-
+                        data_store["iliad"]["shipping"][0] = spedizione;
+                        data_store["iliad"]["shipping"][1] = order_shipped;
+                        data_store["iliad"]["shipping"][2] = tracking_text;
+                        data_store["iliad"]["shipping"][3] = tracking;
+                        data_store["iliad"]["sim"][0] = title;
+                        data_store["iliad"]["sim"][1] = activation;
+                        if (check == undefined) {
+                            data_store["iliad"]["sim"][2] = 'true';
+                        } else {
+                            data_store["iliad"]["sim"][2] = 'false';
+                        }
+                        data_store["iliad"]["sim"][3] = offer;
                         data_store["iliad"]["user"]["user_name"] = user_name;
                         data_store["iliad"]["user"]["user_id"] = user_id;
                         data_store["iliad"]["user"]["user_numtell"] = user_numtell;
-                        data_store["iliad"]["user"]["offer"] = offer;
                         data_store["iliad"]["validation"][0] = validation;
                         data_store["iliad"]["validation"][1] = order_date;
                         data_store["iliad"]["validation"][2] = date;
-                        info_request(data_store);
+                        res.send(data_store)
+                        console.log(data_store);
                     });
                 } catch (Exeption) {
                     res.send(503);
                 }
             }
         });
+    }
+    else if (info == 'true' && token != undefined) {
+        var headers = {
+            'authority': 'www.iliad.it',
+            'cache-control': 'max-age=0',
+            'origin': 'https://www.iliad.it',
+            'upgrade-insecure-requests': '1',
+            'content-type': 'application/x-www-form-urlencoded',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'referer': 'https://www.iliad.it/account/',
+            'accept-language': 'it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7,pt;q=0.6',
+            'cookie': 'ACCOUNT_SESSID=' + token
+        };
+        var options = {
+            url: 'https://www.iliad.it/account/mes-informations',
+            method: 'POST',
+            headers: headers,
+        };
+        request(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                const $ = cheerio.load(body);
+                var results = $('body');
+                var array = [];
+                try {
+                    results.each(function (i, result) {
+                        $(result)
+                            .find('div.infos__content')
+                            .each(function (index, element) {
+                                array = array.concat([$(element).find('div.infos__text').text()]);
+                            });
+                        var puk = $(result).find('span.bulle-info').attr('data-help-content');
 
-        function info_request(data) {
-            var options = {
-                url: 'https://www.iliad.it/account/mes-informations',
-                method: 'POST',
-                headers: headers,
-            };
-            request(options, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    const $ = cheerio.load(body);
-                    var results = $('body');
-                    var array = [];
-                    try {
-                        results.each(function (i, result) {
-                            $(result)
-                                .find('div.infos__content')
-                                .each(function (index, element) {
-                                    array = array.concat([$(element).find('div.infos__text').text()]);
-                                });
-                            var address_title = array[0].split('\n')[1].split('  ').join('');
-                            var address = array[0].split('\n')[3].split('  ').join('');
-                            var cap = array[0].split('\n')[5].split('   ').join('');
-                            var pay_title = array[1].split('\n')[1].split('  ').join('');
-                            var pay_method = array[1].split('\n')[2].split('   ').join('');
-                            var mail_title = array[2].split('\n')[1].split('  ').join('');
-                            var mail = array[2].split('\n')[2].split('  ').join('');
-                            data["iliad"]["address"] = {};
-                            data["iliad"]["pay"] = {};
-                            data["iliad"]["mail"] = {};
-                            data["iliad"]["puk"] = {};
-                            data["iliad"]["address"][0] = address_title;
-                            data["iliad"]["address"][1] = address;
-                            data["iliad"]["address"][2] = cap;
-                            data["iliad"]["pay"][0] = pay_title;
-                            data["iliad"]["pay"][1] = pay_method;
-                            data["iliad"]["mail"][0] = mail_title;
-                            data["iliad"]["mail"][1] = mail;
-                            var puk = $(result)
-                                .find('span.bulle-info').attr('data-help-content');
-                            data["iliad"]["puk"][0] = puk;
-                            get_condition(data);
-                        });
-                    } catch (Exeption) {}
-                }
-            });
-        }
+                        var address_title = array[0].split('\n')[1].split('  ').join('');
+                        var address = array[0].split('\n')[3].split('  ').join('');
+                        var cap = array[0].split('\n')[5].split('   ').join('');
+                        var pay_title = array[1].split('\n')[1].split('  ').join('');
+                        var pay_method = array[1].split('\n')[2].split('   ').join('');
+                        var mail_title = array[2].split('\n')[1].split('  ').join('');
+                        var mail = array[2].split('\n')[2].split('  ').join('');
+                        var password_title = array[3].split('\n')[1].split('  ').join('');
+                        var password = array[3].split('\n')[2].split('  ').join('');
+                        var puk_title = array[4].split('\n')[3].split('     ').join('');
+                        var puk_text = array[4].split('\n')[7].split('   ').join('');
 
-        function get_condition(data) {
-            var options = {
-                url: 'https://www.iliad.it/account/mes-conditions',
-                method: 'POST',
-                headers: headers,
-            };
-            request(options, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    const $ = cheerio.load(body);
-                    var results = $('body');
-                    var array = [];
-                    var array2 = [];
-                    try {
-                        results.each(function (i, result) {
-                            $(result)
-                                .find('div.conso__content')
-                                .each(function (index, element) {
-                                    array = array.concat([$(element).find('div.conso__text').text()]);
-                                });
-                            var condition_title = array[0].split('\n')[1].split('   ').join('')
-                            var condition_text = array[0].split('\n')[2].split('   ').join('')
-                            var price_title = array[1].split('\n')[1].split('   ').join('')
-                            var price_text = array[1].split('\n')[2].split('   ').join('')
-                            $(result)
-                                .find('div.conso__content')
-                                .each(function (index, element) {
-                                    array2 = array2.concat([$(element).find('div.conso__text').find('a').attr('href')]);
-                                });
-                            var condition_doc = 'https://www.iliad.it' + array2[0];
-                            var price_doc = 'https://www.iliad.it' + array2[1];
-                            data["iliad"]["condition"] = {};
-                            data["iliad"]["price"] = {};
-                            data["iliad"]["condition"][0] = condition_title;
-                            data["iliad"]["condition"][1] = condition_text;
-                            data["iliad"]["condition"][2] = condition_doc;
-                            data["iliad"]["price"][0] = price_title;
-                            data["iliad"]["price"][1] = price_text;
-                            data["iliad"]["price"][2] = price_doc;
-                            res.send(data);
-                            console.log(data);
-                        });
-                    } catch (Exeption) {}
-                }
-            });
-        }
+                        data_store["iliad"]["address"] = {};
+                        data_store["iliad"]["pay"] = {};
+                        data_store["iliad"]["mail"] = {};
+                        data_store["iliad"]["puk"] = {};
+                        data_store["iliad"]["password"] = {};
+
+                        data_store["iliad"]["address"][0] = address_title;
+                        data_store["iliad"]["address"][1] = address;
+                        data_store["iliad"]["address"][2] = cap;
+                        data_store["iliad"]["pay"][0] = pay_title;
+                        data_store["iliad"]["pay"][1] = pay_method;
+                        data_store["iliad"]["mail"][0] = mail_title;
+                        data_store["iliad"]["mail"][1] = mail;
+                        data_store["iliad"]["password"][0] = password_title;
+                        data_store["iliad"]["password"][1] = password;
+                        data_store["iliad"]["puk"][0] = puk_title;
+                        data_store["iliad"]["puk"][1] = puk_text;
+                        data_store["iliad"]["puk"][2] = puk;
+
+                        res.send(data_store);
+                        console.log(data_store);
+                    });
+                } catch (Exeption) { }
+            }
+        });
+    }
+    else if (doc == 'true' && token != undefined) {
+        var headers = {
+            'authority': 'www.iliad.it',
+            'cache-control': 'max-age=0',
+            'origin': 'https://www.iliad.it',
+            'upgrade-insecure-requests': '1',
+            'content-type': 'application/x-www-form-urlencoded',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'referer': 'https://www.iliad.it/account/',
+            'accept-language': 'it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7,pt;q=0.6',
+            'cookie': 'ACCOUNT_SESSID=' + token
+        };
+        var options = {
+            url: 'https://www.iliad.it/account/mes-conditions',
+            method: 'POST',
+            headers: headers,
+        };
+        request(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                const $ = cheerio.load(body);
+                var results = $('body');
+                var array = [];
+                var array2 = [];
+                try {
+                    results.each(function (i, result) {
+                        $(result)
+                            .find('div.conso__content')
+                            .each(function (index, element) {
+                                array = array.concat([$(element).find('div.conso__text').text()]);
+                            });
+                        $(result)
+                            .find('div.conso__content')
+                            .each(function (index, element) {
+                                array2 = array2.concat([$(element).find('div.conso__text').find('a').attr('href')]);
+                            });
+                        var condition_title = array[0].split('\n')[1].split('   ').join('')
+                        var condition_text = array[0].split('\n')[2].split('   ').join('')
+                        var price_title = array[1].split('\n')[1].split('   ').join('')
+                        var price_text = array[1].split('\n')[2].split('   ').join('')
+                        var condition_doc = 'https://www.iliad.it' + array2[0];
+                        var price_doc = 'https://www.iliad.it' + array2[1];
+
+                        data_store["iliad"]["condition"] = {};
+                        data_store["iliad"]["price"] = {};
+                        data_store["iliad"]["condition"][0] = condition_title;
+                        data_store["iliad"]["condition"][1] = condition_text;
+                        data_store["iliad"]["condition"][2] = condition_doc;
+                        data_store["iliad"]["price"][0] = price_title;
+                        data_store["iliad"]["price"][1] = price_text;
+                        data_store["iliad"]["price"][2] = price_doc;
+                        res.send(data_store);
+                        console.log(data_store);
+                    });
+                } catch (Exeption) { }
+            }
+        });
     }
 });
 exports = module.exports = app;
-const server = app.listen(process.env.PORT, function () {});
+const server = app.listen(process.env.PORT, function () { });
